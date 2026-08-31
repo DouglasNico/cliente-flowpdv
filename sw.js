b84bc9df00576a304c7d7981a3906d6d81cb906c
DouglasNico <dougnvds26@gmail.com>
2026-08-31 13:05:04 -0300

    feat: barra de rolagem estilizada personalizada em todo o app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// FlowPDV Mobile Service Worker v1.4.3 - Ultra-Fast PWA
-const CACHE_NAME = 'flowpdv-mobile-v1.4.3';
+// FlowPDV Mobile Service Worker v1.4.4 - Ultra-Fast PWA
+const CACHE_NAME = 'flowpdv-mobile-v1.4.4';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
