6cb0068bea3d81b068a289a28d8f62e4e65d0aff
DouglasNico <dougnvds26@gmail.com>
2026-09-15 20:29:55 -0300

    fix: sharpen PWA header and prevent double-tap zoom

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // FlowPDV Mobile Service Worker v2.2.22 - Ultra-Fast PWA
-const CACHE_NAME = 'flowpdv-mobile-v20260915b';
+const CACHE_NAME = 'flowpdv-mobile-v20260915c';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
